80b72693999ea058d4e5129dc22694af92a01f78
Wathanyoo99 <klol5837@gmail.com>
2025-09-05 21:53:16 +0700

    up urltrans

--- wallet/urls.py
+++ wallet/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
+    path('delete_transaction/<int:transaction_id>/', views.delete_transaction, name='delete_transaction'),
 ]
