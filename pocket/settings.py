cf0cfc587ed4b54f25f211adcb137fbce6782b4a
bXiilzy <jiranankredkla12@gmail.com>
2025-09-06 00:49:43 +0700

    fix database

--- pocket/settings.py
+++ pocket/settings.py
@@ -84,10 +84,9 @@ LOGIN_REDIRECT_URL = "/login/"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # ใช้ os.path.join
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
