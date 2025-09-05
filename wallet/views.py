9dc3cd402e5b726092a3fac507318815489364a8
bXiilzy <jiranankredkla12@gmail.com>
2025-09-05 17:08:21 +0700

    import

--- wallet/views.py
+++ wallet/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest
+from django.contrib.auth import authenticate, login, logout
+from .models import Transaction
+from decimal import Decimal
+from django.core.serializers.json import DjangoJSONEncoder
+import json
+from django.contrib.auth.models import User
 # Create your views here.
 
 def register(request):
